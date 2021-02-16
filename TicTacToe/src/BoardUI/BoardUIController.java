18db4eb5f71848fd4e7a124a208b3746c10e0828
Master Sina <sinaha3030@gmail.xom>
2021-02-15 20:24:25 -0800

    added img view for now for the boxes

--- TicTacToe/src/BoardUI/BoardUIController.java
+++ TicTacToe/src/BoardUI/BoardUIController.java
@@ -1,6 +1,9 @@
 package BoardUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class BoardUIController
 {
-
+    //@FXML private ImageView position[3][3] = {{button},{},{}};
 }
