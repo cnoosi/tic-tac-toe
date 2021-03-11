7d5c96ffe9421a901fffef37982a71e36f1bf3b0
Dapale-cpp <dapalejohnmisc@gmail.com>
2021-03-10 16:41:44 -0800

    Update MenuUIController.java

--- src/MenuUI/MenuUIController.java
+++ src/MenuUI/MenuUIController.java
@@ -3,6 +3,8 @@ package MenuUI;
 import Game.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -16,13 +18,16 @@ public class MenuUIController
     public void handleSinglePlayerMode(ActionEvent event) throws Exception
     {
         Stage stage = (Stage) singlePlayerBtn.getScene().getWindow();
-        openScene.start(stage, "/BoardUI/BoardUI.fxml", "Tic-Tac-Toe - Game");
+        openScene.start(stage, "/BoardUI/BoardUI.fxml", "Tic-Tac-Toe - Single Player Game");
+        //Somehow get boardui controller so we can change local player # to 1
     }
 
     @FXML
     public void handleTwoPlayerMode(ActionEvent event) throws Exception
     {
         Stage stage = (Stage) twoPlayerBtn.getScene().getWindow();
-        openScene.start(stage, "/BoardUI/BoardUI.fxml", "Tic-Tac-Toe - Game");
+        openScene.start(stage, "/BoardUI/BoardUI.fxml", "Tic-Tac-Toe - Two Player Game");
+        Parent root = FXMLLoader.load(getClass().getResource("/BoardUI/BoardUI.fxml"));
+        //Somehow get boardui controller so we can change local player # to 2
     }
 }
