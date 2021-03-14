90c7824c47970eab1f2b482860167d6ece5205e5
Dapale-cpp <dapalejohnmisc@gmail.com>
2021-03-13 23:07:43 -0800

    Created GameState class
    Data sent by the server to clients so they can determine what UI to display and where the game is in its current state

---
+++ src/Game/GameState.java
@@ -0,0 +1,32 @@
+package Game;
+
+public class GameState {
+    private boolean isActive = false;
+    private int currentWinner = -1;
+    private int currentToken = -1;
+
+    public GameState() {}
+    public GameState(boolean isActive, int currentWinner, int currentToken)
+    {
+        this.isActive = isActive;
+        this.currentWinner = currentWinner;
+        this.currentToken = currentToken;
+    }
+
+    public void setActive(boolean isActive)
+    {
+        this.isActive = isActive;
+    }
+
+    public void setCurrentWinner(int currentWinner)
+    {
+        this.currentWinner = currentWinner;
+    }
+
+    public void setCurrentToken(int currentToken)
+    {
+        this.currentToken = currentToken;
+    }
+
+    
+}
--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,4 +1,22 @@
 package Game;
 
+import java.util.ArrayList;
+
 public class Player {
+    private String name;
+    private int winCount;
+    private GameState currentGameState;
+    private ArrayList<Player> players;
+    private int[][] boardData;
+    private int token;
+
+    public Player()
+    {
+
+    }
+
+    public boolean sendMoveRequest(int i, int j)
+    {
+        return true;
+    }
 }
