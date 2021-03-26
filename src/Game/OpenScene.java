837cf6e760db789e07aef55ca2bb934bcef40c09
kasra <little_kasra@yahoo.com>
2021-03-25 23:56:45 -0700

    created a music player class
    not fully functional yet need to figure out how to call the music objects function from different classes

--- src/Game/OpenScene.java
+++ src/Game/OpenScene.java
@@ -4,15 +4,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class OpenScene {
 
     public void start(Stage window, String fxmlFile, String headerName) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
         window.setTitle(headerName);
         window.setResizable(false);
         window.setScene(new Scene(root));
         window.show();
+        String musicPath = "/Users/kasra/Documents/GitHub/TicTacToe/tic-tac-toe/src/resources/images/soundTrack.wav";
+        MusicPlayer music = new MusicPlayer(musicPath);
+        music.playMusic();
+        music.stopMusic();
     }
 
     public void start(Stage window, Parent root, String headerName) throws Exception {
@@ -20,5 +28,9 @@ public class OpenScene {
         window.setResizable(false);
         window.setScene(new Scene(root));
         window.show();
+        String musicPath = "/Users/kasra/Documents/GitHub/TicTacToe/tic-tac-toe/src/resources/images/soundTrack.wav";
+        MusicPlayer music = new MusicPlayer(musicPath);
+        music.playMusic();
+        music.stopMusic();
     }
 }
