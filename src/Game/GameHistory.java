7bed1f180e979b9cfcc853f0fb288374163cfbb8
Parsa <parsa.bavafa0@gmail.com>
2021-05-10 17:52:53 -0700

    Updated GameHistory.java
    added getters and setters

--- src/Game/GameHistory.java
+++ src/Game/GameHistory.java
@@ -14,15 +14,66 @@ public class GameHistory {
 
         public GameHistory(String gameId, int player1Id, int player2Id, int winnerToken, long startTime, long endTime, int[][] gameMoves){
                 setGameId(gameId);
-                setplayer1Id(player1Id);
+                setPlayer1Id(player1Id);
                 setPlayer2Id(player2Id);
                 setWinnerToken(winnerToken);
                 setStartTime(startTime);
-                setGameMoves();
+                setGameMoves(gameMoves);
         }
 
-        public void saveGameHistory(GameProcess game){
-
+        public String getGameId() {
+                return gameId;
         }
 
+        public void setGameId(String gameId) {
+                this.gameId = gameId;
+        }
+
+        public int getPlayer1Id() {
+                return player1Id;
+        }
+
+        public void setPlayer1Id(int player1Id) {
+                this.player1Id = player1Id;
+        }
+
+        public int getPlayer2Id() {
+                return player2Id;
+        }
+
+        public void setPlayer2Id(int player2Id) {
+                this.player2Id = player2Id;
+        }
+
+        public int getWinnerToken() {
+                return winnerToken;
+        }
+
+        public void setWinnerToken(int winnerToken) {
+                this.winnerToken = winnerToken;
+        }
+
+        public long getStartTime() {
+                return startTime;
+        }
+
+        public void setStartTime(long startTime) {
+                this.startTime = startTime;
+        }
+
+        public long getEndTime() {
+                return endTime;
+        }
+
+        public void setEndTime(long endTime) {
+                this.endTime = endTime;
+        }
+
+        public int[][] getGameMoves() {
+                return gameMoves;
+        }
+
+        public void setGameMoves(int[][] gameMoves) {
+                this.gameMoves = gameMoves;
+        }
 }
