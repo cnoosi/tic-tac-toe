0a64ed056916eeed245ca60220cf362eb4b11216
kasra <37860723+kasrs@users.noreply.github.com>
2021-05-27 20:24:13 -0700

    added User preferences UI

---
+++ src/UserControlsUI/UserPrefUIController.java
@@ -0,0 +1,14 @@
+package UserControlsUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserPrefUIController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
