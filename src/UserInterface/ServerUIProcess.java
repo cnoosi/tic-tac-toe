1969b7c7f696220d34ad271221c605ee8153d288
Master Sina <sinaha3030@gmail.xom>
2021-05-28 02:02:06 -0700

    dash update

--- src/UserInterface/ServerUIProcess.java
+++ src/UserInterface/ServerUIProcess.java
@@ -1,11 +1,42 @@
 package UserInterface;
 
+import AdminDashUI.AdminDashUIController;
+import MenuUI.MenuUIController;
 import Networking.ServerProcess;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public class ServerUIProcess
 {
+    ServerProcess serverProcess;
+    Stage primaryStage;
+    Parent adminDashRoot;
+    AdminDashUIController adminController;
+    FXMLLoader adminLoader;
+
     public ServerUIProcess(ServerProcess serverProcess, Stage primaryStage)
+    {
+        this.serverProcess = serverProcess;
+        this.primaryStage  = primaryStage;
+        load();
+    }
+
+    public void load()
+    {
+        try
+        {
+            adminLoader = new FXMLLoader(getClass().getResource("/AdminDashUI/AdminDashUI.fxml"));
+            adminDashRoot = adminLoader.load();
+            adminController = adminLoader.getController();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void openPage(String page)
     {
 
     }
